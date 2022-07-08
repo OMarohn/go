a06e319bffdfc729c4e868368818f478b595bf74
OMarohn <omarohn@gmail.com>
2022-07-08 10:21:34 +0200

    renaming

--- src/kom.com/coaster/coaster/service_test.go
+++ src/kom.com/coaster/coaster/service_test.go
@@ -7,40 +7,40 @@ import (
 )
 
 func TestCoasterService(t *testing.T) {
-	var s CoasterService = NewCoasterService(NewCoasterMemmoryRepo())
+	var coasterService CoasterService = NewCoasterService(NewCoasterMemmoryRepo())
 	var testCoaster = Coaster{ID: "id123", Name: "TestCoaster", Manufacture: "TestManufature", Height: 123}
 	var assert = assert.New(t)
 
 	t.Run("Initial leer", func(t *testing.T) {
-		c := s.getCoasters()
+		c := coasterService.getCoasters()
 		assert.Len(c, 0)
 	})
 
 	t.Run("Anlegen eines leeren Coasters", func(t *testing.T) {
-		err := s.createCoaster(Coaster{})
+		err := coasterService.createCoaster(Coaster{})
 		assert.Error(err)
 		assert.EqualError(err, "id fehlt")
 	})
 
 	t.Run("Anlegen eines vollständigen Coasters", func(t *testing.T) {
-		err := s.createCoaster(testCoaster)
+		err := coasterService.createCoaster(testCoaster)
 		assert.NoError(err)
 	})
 
 	t.Run("Lesen des angelegten Coasters", func(t *testing.T) {
-		c, err := s.getCoaster("id123")
+		c, err := coasterService.getCoaster("id123")
 		assert.NoError(err)
 		assert.Equal(c, testCoaster)
 	})
 
 	t.Run("Löschen eines unbekannten Coasters", func(t *testing.T) {
-		err := s.deleteCoaster("id9999")
+		err := coasterService.deleteCoaster("id9999")
 		assert.Error(err)
 		assert.EqualError(err, "datensatz nicht gefunden")
 	})
 
 	t.Run("Löschen des angelegten Coasters", func(t *testing.T) {
-		err := s.deleteCoaster("id123")
+		err := coasterService.deleteCoaster("id123")
 		assert.NoError(err)
 	})
 
